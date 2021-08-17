ea613b1ddcd6d547799590216de2e62b698b88c2
GUILHERME FERNANDES NETO <40431403+gangagoura@users.noreply.github.com>
2021-08-17 10:22:31 -0300

    Create contaPoupanca.java

---
+++ src/contaPoupanca.java
@@ -0,0 +1,15 @@
+
+public class ContaPoupanca extends Conta {
+
+	public ContaPoupanca(Cliente cliente) {
+		super(cliente);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void imprimirExtrato() {
+		System.out.println("=== Extrato Conta Poupança ===");
+		super.imprimirInfosComuns();
+	}
+
+}
